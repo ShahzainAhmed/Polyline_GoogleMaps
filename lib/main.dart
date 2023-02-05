e562a3fcb55daa4a5178d93a477f11c60bbb575b
ShahzainAhmed <shahzainahmed57@gmail.com>
2023-02-05 23:18:04 +0500

    commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:fluttergooglemap/convert_latlang_to_address.dart';
+import 'package:fluttergooglemap/custom_marker_info_window.dart';
+import 'package:fluttergooglemap/custom_marker_screen.dart';
+import 'package:fluttergooglemap/google_places_api.dart';
+import 'package:fluttergooglemap/home.dart';
+import 'package:fluttergooglemap/polygons.dart';
+import 'package:fluttergooglemap/polyline.dart';
+import 'package:fluttergooglemap/users_current_location.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: PolylineScreen(),
+    );
+  }
+}
